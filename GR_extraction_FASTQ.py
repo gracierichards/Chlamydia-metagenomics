07116ff2ef6ca55486721e68307214b70bd2c629
gracierichards <50161739+gracierichards@users.noreply.github.com>
2021-02-24 14:07:33 -0800

    Working on faster extraction
    Finished writing a program that extracts all species in one round. 2 versions for with quality scores and without.

---
+++ GR_extraction_FASTQ.py
@@ -0,0 +1,187 @@
+from Bio import SeqIO
+import argparse
+import os
+import sys
+
+"""Save this program in an empty directory that will contain the reads for a single sample. Also run this program from the directory
+it is in."""
+
+species_set = {}
+nonspecies = {}
+clost = []
+zoonotic = ["Anaplasma phagocytophilum", "Bacillus anthracis", "Bacillus paranthracis", "Borreliella burgdorferi", "Brucella abortus",
+            "Brucella melitensis", "Brucella sp. 10RB9215", "Brucella suis", "Campylobacter avium", "Campylobacter coli",
+            "Campylobacter jejuni", "Campylobacter ureolyticus", "Lumpy skin disease virus", "Enterococcus avium",
+            "Erysipelothrix rhusiopathiae", "Francisella tularensis", "Haemophilus influenzae", "Haemophilus parahaemolyticus",
+            "Haemophilus parainfluenzae", "Mycobacterium avium", "Mycobacterium chimaera", "Mycobacterium colombiense",
+            "Mycobacterium intracellulare", "Mycobacterium lepraemurium", "Mycobacterium mantenii", "Mycobacterium marseillense",
+            "Mycobacterium paraintracellulare", "Streptococcus suis", "Yersinia pestis", "Yersinia pseudotuberculosis"]
+bv = ["Anaerococcus", "Atopobium", "Bifidobacterium", "Corynebacterium", "Enterobacter", "Finegoldia", "Gemella", "Megasphaera",
+      "Mobiluncus", "Peptoniphilus", "Prevotella", "Staphylococcus", "Streptococcus"]
+pseudomonas = ["aeruginosa", "alcaligenes", "citronellolis", "furukawaii", "mendocina", "multiresinivorans", "nitroreducens",
+               "oleovorans", "resinovorans"]
+std = ["Acinetobacter junii", "Human alphaherpesvirus 2", "Mycoplasma genitalium", "Neisseria gonorrhoeae", "Ureaplasma urealyticum"]
+gut = ["Bacteroides fragilis", "Clostridioides difficile", "Enterococcus faecalis", "Enterococcus faecium", "Helicobacter pylori",
+       "Rotavirus C", "Rotavirus D", "Salmonella enterica", "Vibrio cholerae", "Vibrio parahaemolyticus",
+       "Yersinia enterocolitica", "Yersinia similis"]
+burkholderia = ["mallei", "oklahomensis", "pseudomallei", "sp. BDU6", "sp. BDU8", "sp. MSMB0266", "sp. MSMB0852", "sp. MSMB1588",
+                "sp. MSMB617WGS", "thailandensis"]
+skin = ["Lymphocystis disease virus 1", "Mycobacterium haemophilum", "Mycobacterium ulcerans", "Canid alphaherpesvirus 1",
+        "Equid alphaherpesvirus 9", "Suid alphaherpesvirus 1"]
+bacillus = ["albus", "bombysepticus", "cereus", "cytotoxicus", "luti", "mobilis", "mycoides", "pacificus", "pseudomycoides",
+            "sp. ABP14", "sp. FDAARGOS_235", "sp. HBCD-sjtu", "sp. JAS24-2", "thuringiensis", "toyonensis", "tropicus", "wiedmannii"]
+respiratory = ["Influenza A virus", "Bordetella parapertussis", "Bordetella pertussis", "Dialister pneumosintes",
+               "Elizabethkingia anophelis", "Haemophilus haemolyticus", "Haemophilus pittmaniae", "Haemophilus sp. oral taxon 036",
+               "Klebsiella aerogenes", "Klebsiella pneumoniae", "Legionella pneumophila", "Mycobacterium haemophilum",
+               "Mycobacterium leprae", "Mycobacterium canettii", "Mycobacterium tuberculosis", "Mycoplasma pneumoniae"]
+acinetobacter = ["baumannii", "calcoaceticus", "nosocomialis", "pittii"]
+
+def find_species_name(out_taxid):
+    report = open("kraken_report_clean_" + n.sample + ".txt", "r")
+    for line in report:
+        columns = line.split("\t")
+        if columns[4] == out_taxid:
+            taxon = " ".join(columns[5].split())
+            if columns[3] == "S":
+                species_set[out_taxid] = taxon
+            else:
+                nonspecies[out_taxid] = taxon
+            report.close()
+            return taxon
+    
+def pathogen_type(species):
+    if species.split()[0] == "Lactobacillus":
+        return "Lactobacillus"
+    elif species.split()[0] == "Gardnerella":
+        return "Gardnerella"
+    elif species.split()[0] == "Rickettsia":
+        return "Zoonotic_pathogens"
+    elif species.split()[0] == "Chlamydia":
+        return "Chlamydia"
+    elif species.split()[0] == "Waddlia":
+        return "Chlamydia_like"
+    elif species.split()[0].endswith("chlamydia"):
+        return "Chlamydia_like"
+    elif species.split()[0] == "Simkania":
+        return "Chlamydia_like"
+    elif species.split()[0] == "Coxiella":
+        return "Zoonotic_pathogens"
+    elif species.split()[0] == "Leptospira":
+        return "Zoonotic_pathogens"
+    elif species.split()[0] == "Orientia":
+        return "Zoonotic_pathogens"
+    elif species.startswith("Streptococcus sp."):
+        return "Zoonotic_pathogens"
+    elif species == "Escherichia coli":
+        return "BV_pathogens"
+    elif species.startswith("Coriobacteri"):
+        return "BV_pathogens"
+    elif species == "Collinella aerofaciens":
+        return "BV_pathogens"
+    elif species.split()[0] == "Alphapapillomavirus":
+        return "STD_pathogens"
+    elif species.split()[0] == "Pseudomonas" and species.split()[1] in pseudomonas:
+        return "STD_pathogens"
+    elif species.split()[0] == "Shigella":
+        return "Gut_pathogens"
+    elif species.split()[0] == "Burkholderia" and species.split()[1:] in burkholderia:
+        return "Skin_pathogens"
+    elif species == "Listeria monocytogenes":
+        return "Foodborne_pathogens"
+    elif species.split()[0] == "Bacillus" and species.split()[1:] in bacillus:
+        return "Foodborne_pathogens"
+    elif species.split()[0] == "Acinetobacter" and species.split()[1] in acinetobacter:
+        return "ARB"
+    elif species in zoonotic:
+        return "Zoonotic_pathogens"
+    elif species in clost:
+        return "BV_pathogens"
+    elif species.split()[0] in bv:
+        return "BV_pathogens"
+    elif species in std:
+        return "STD_pathogens"
+    elif species in gut:
+        return "Gut_pathogens"
+    elif species in skin:
+        return "Skin_pathogens"
+    elif species in respiratory:
+        return "Respiratory_pathogens"
+    else:
+        return "Other"
+    
+"""Goes through the Kraken report and lists out the taxids of all species within the order Clostridiales."""
+def make_list_clost_species():
+    list1 = []
+    found = False
+    for line in report:
+        columns = line.split("\t")
+        if found:
+            if columns[3] == "O":
+                global clost
+                clost = list1
+                report.close()
+                return
+            elif columns[3] == "S":
+                species = " ".join(columns[5].split())
+                list1.append(species)
+        else:
+            if columns[4] == "186802":
+                found = True
+    print("Error: Did not find the end of Clostridiales in Kraken report")
+    
+def make_directories(pathogen):
+    try:
+        os.mkdir(os.getcwd() + "/" + pathogen)
+    except FileExistsError:
+        pass
+    
+parser = argparse.ArgumentParser()
+parser.add_argument("sample")
+n = parser.parse_args()
+try:
+    kraken_out = open("/home/sbomman/Kraken2/analysis_output/" + n.sample + "_kraken.out", "r")
+    report = open("/home/sbomman/Kraken2/output/kraken_report_clean_" + n.sample + ".txt", "r")
+    records = SeqIO.index("/home/sbomman/Kneaddata/knead_" + n.sample + "/kneaddata_output/" + n.sample + "_trimmed_grouped_kneaddata.fastq", "fastq")
+except IOError:
+    print("Not a valid sample name.")
+    sys.exit()
+
+make_list_clost_species()
+
+for pathogen in ["Lactobacillus", "Gardnerella", "Chlamydia", "Chlamydia_like", "Zoonotic_pathogens", "BV_pathogens", "STD_pathogens",
+                 "Gut_pathogens", "Skin_pathogens", "Foodborne_pathogens", "Respiratory_pathogens", "ARB", "Other"]:
+    make_directories(pathogen)
+
+line = kraken_out.readline()
+while line != "":
+    if line[0] != "U":
+        read_id = line.split("\t")[1]
+        taxid = line.split("\t")[2]
+        if taxid != "0" and taxid != "1":
+            if taxid in species_set:
+                species = species_set[taxid]
+                record = records[read_id]
+                target_dir = pathogen_type(species)
+                species = species.replace(":", "")
+                species = species.replace("(", "")
+                species = species.replace(")", "")
+                species = species.replace(" ", "_")
+                f = open(os.getcwd() + "/" + target_dir + "/" + species + ".txt", "a")
+                SeqIO.write(record, f, "fastq")
+                f.close()
+            elif taxid in nonspecies:
+                continue
+            else:
+                species = find_species_name(taxid)
+                if taxid in species_set:
+                    record = records[read_id]
+                    target_dir = pathogen_type(species)
+                    species = species.replace(":", "")
+                    species = species.replace("(", "")
+                    species = species.replace(")", "")
+                    species = species.replace(" ", "_")
+                    f = open(os.getcwd() + "/" + target_dir + "/" + species + ".txt", "a")
+                    SeqIO.write(record, f, "fastq")
+                    f.close()
+    line = kraken_out.readline()
+    
